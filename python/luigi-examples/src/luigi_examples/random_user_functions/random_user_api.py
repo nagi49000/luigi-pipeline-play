a1eee199c10d01666d0bbed644ee898c5a2aa48c
nagi <nagi>
2024-05-25 16:20:13 +0100

    refactor of file functions to wrap generator functions

--- python/luigi-examples/src/luigi_examples/random_user_functions/random_user_api.py
+++ python/luigi-examples/src/luigi_examples/random_user_functions/random_user_api.py
@@ -1,6 +1,8 @@
 import requests
 import json
 import os
+from logging import Logger
+from typing import Generator
 from pathlib import Path
 from jsonschema import validate, ValidationError
 from json.decoder import JSONDecodeError
@@ -9,17 +11,31 @@ from datetime import datetime, UTC
 from string import ascii_uppercase
 
 
-def download_random_users_to_file(logger, output_path, n_record=3):
+def download_random_users(logger: Logger, n_record: int) -> Generator[str, None, None]:
+    for _ in range(n_record):
+        response = requests.get("https://randomuser.me/api/")
+        if response.ok:
+            yield response.text + "\n"
+        else:
+            logger.error(
+                f"GET to https://randomuser.me/api/ failed with {response.status_code}: {response.reason}"
+            )
+
+
+def download_random_users_to_file(logger: Logger, output_path: str, n_record: int = 3):
     with open(output_path, "wt") as f:
-        for _ in range(n_record):
-            response = requests.get("https://randomuser.me/api/")
-            if response.ok:
-                f.write(response.text)
-                f.write("\n")
-            else:
-                logger.error(
-                    f"GET to https://randomuser.me/api/ failed with {response.status_code}: {response.reason}"
-                )
+        for line in download_random_users(logger, n_record):
+            f.write(line)
+
+
+def validate_random_users(logger: Logger, input_generator: Generator[str, None, None], schema: dict) -> Generator[tuple[int, str], None, None]:
+    for line in input_generator:
+        try:
+            validate(instance=json.loads(line), schema=schema)
+            yield (0, line)
+        except (ValidationError, JSONDecodeError) as e:
+            logger.error(e)
+            yield (1, line)
 
 
 def validate_random_users_to_file(
@@ -181,10 +197,11 @@ def validate_random_users_to_file(
 
     with open(valid_output_path, "wt") as valid_output_lines:
         with open(invalid_output_path, "wt") as invalid_output_lines:
-            for line in input_generator:
-                try:
-                    validate(instance=json.loads(line), schema=schema)
-                    valid_output_lines.write(line)
-                except (ValidationError, JSONDecodeError) as e:
-                    logger.error(e)
-                    invalid_output_lines.write(line)
+            for line in validate_random_users(logger, input_generator, schema):
+                stream_index, record = line
+                if stream_index == 0:
+                    valid_output_lines.write(record)
+                elif stream_index == 1:
+                    invalid_output_lines.write(record)
+                else:
+                    logger.error(f"encountered unknown stream_index {stream_index}")
--- python/luigi-examples/src/luigi_examples/random_user_functions/random_user_to_file.py
+++ python/luigi-examples/src/luigi_examples/random_user_functions/random_user_to_file.py
@@ -2,29 +2,34 @@ import json
 import avro.schema
 from avro.datafile import DataFileWriter
 from avro.io import DatumWriter
+from typing import Generator
 import pyarrow
 import pyarrow.parquet
 
 
-def extract_flat_details_to_file(_, input_generator, output_path):
+def extract_flat_details(_, input_generator: Generator[str, None, None]) -> Generator[str, None, None]:
+    for line in input_generator:
+        in_row = json.loads(line)["results"][0]
+        out_row = {
+            "name": in_row["name"]["first"] + " " + in_row["name"]["last"],
+            "gender": in_row["gender"],
+            "phone": in_row["phone"],
+            "cell": in_row["cell"],
+            "email": in_row["email"],
+            "city": in_row["location"]["city"],
+            "state": in_row["location"]["state"],
+            "country": in_row["location"]["country"],
+        }
+        yield json.dumps(out_row) + "\n"
+
+
+def extract_flat_details_to_file(_, input_generator: Generator[str, None, None], output_path: str):
     with open(output_path, "wt") as output_lines:
-        for line in input_generator:
-            in_row = json.loads(line)["results"][0]
-            out_row = {
-                "name": in_row["name"]["first"] + " " + in_row["name"]["last"],
-                "gender": in_row["gender"],
-                "phone": in_row["phone"],
-                "cell": in_row["cell"],
-                "email": in_row["email"],
-                "city": in_row["location"]["city"],
-                "state": in_row["location"]["state"],
-                "country": in_row["location"]["country"],
-            }
-            output_lines.write(json.dumps(out_row))
-            output_lines.write("\n")
+        for line in extract_flat_details(_, input_generator):
+            output_lines.write(line)
 
 
-def to_avro_file(_, input_generator, output_path):
+def to_avro_file(_, input_generator: Generator[str, None, None], output_path: str):
     avro_schema = {
         "namespace": "randomusers",
         "type": "record",
@@ -49,7 +54,7 @@ def to_avro_file(_, input_generator, output_path):
         writer.close()
 
 
-def to_parquet_file(_, input_generator, output_path):
+def to_parquet_file(_, input_generator: Generator[str, None, None], output_path: str):
     parquet_schema = pyarrow.schema(
         [
             ("name", pyarrow.string()),
