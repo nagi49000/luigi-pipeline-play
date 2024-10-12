64dc6bcbd28c522e7012da997d92a38ef473d373
nagi <nagi>
2024-10-12 20:38:16 +0100

    got suite working

--- python/luigi-examples/scripts/great_expectations_on_flat_ndjson.py
+++ python/luigi-examples/scripts/great_expectations_on_flat_ndjson.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import great_expectations as gx
 
-
+# based on this GX datasource and batch - https://discourse.greatexpectations.io/t/configure-datasource-for-json-files/121
 context = gx.get_context()
 # the data source specifies the engine used for sourcing the data
 data_source = context.data_sources.add_pandas("pandas")
@@ -9,13 +9,30 @@ data_source = context.data_sources.add_pandas("pandas")
 # these are typically some data store/file that the above data source engine can use
 ndjson_filename = Path(__file__).parents[1] / "test_luigi_examples" / "data" / "flat-randomusers.txt"
 data_asset = data_source.add_json_asset(name="ndjson asset on pandas", path_or_buf=ndjson_filename, lines=True)
+
+# define the expectations in code
+suite = gx.ExpectationSuite(name="flat ndjson suite")
+suite.add_expectation(
+    gx.expectations.ExpectColumnValuesToMatchRegex(
+        column="email", regex=r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+    )
+)
+suite.add_expectation(
+    expectation=gx.expectations.ExpectColumnValuesToMatchRegex(
+        column="phone", regex=r"^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$"
+    )
+)
+suite = context.suites.add(suite)
+
 # Batches are designed to be "MECE" -- mutually exclusive and collectively exhaustive partitions of Data Assets
 batch_definition = data_asset.add_batch_definition_whole_dataframe("batch definition")
 batch = batch_definition.get_batch()
 
-expectation = gx.expectations.ExpectColumnValuesToMatchRegex(
-    column="email", regex=r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
-)
+# run validation of expectations against batch of data
+validator = context.get_validator(batch=batch, expectation_suite=suite)
+results = validator.validate()
 
-validation_result = batch.validate(expectation)
-print(validation_result)
+# the expectation suite can be saved as a json configuration
+# validator.save_expectation_suite("expectation_suite.json")
+
+print(results)
