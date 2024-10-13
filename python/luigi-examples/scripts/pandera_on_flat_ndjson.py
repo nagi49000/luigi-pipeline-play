ac9915d6c8b1b5b98bf90d535cf028f293200b53
nagi <nagi>
2024-10-13 12:20:29 +0100

    got panderas working on a flat polars dataframe

--- python/luigi-examples/scripts/pandera_on_flat_ndjson.py
+++ python/luigi-examples/scripts/pandera_on_flat_ndjson.py
@@ -1,6 +1,13 @@
 from pathlib import Path
 import pandera.polars as pa
 import polars as pl
+import re
+
+
+# examples from https://khuyentran1401.github.io/reproducible-data-science/testing_data/pandera.html
+# and https://pandera.readthedocs.io/en/stable/polars.html
+# and https://pandera.readthedocs.io/en/stable/lazy_validation.html
+# and https://pandera.readthedocs.io/en/stable/checks.html
 
 
 schema = pa.DataFrameSchema(
@@ -8,8 +15,8 @@ schema = pa.DataFrameSchema(
         "name": pa.Column(str),
         "gender": pa.Column(str),
         "phone": pa.Column(str),
-        "cell": pa.Column(str),
-        "email": pa.Column(str),
+        "cell": pa.Column(str, pa.Check.str_matches(r"^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$")),
+        "email": pa.Column(str, pa.Check.str_matches(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")),
         "city": pa.Column(str),
         "state": pa.Column(str),
         "country": pa.Column(str),
@@ -19,15 +26,15 @@ schema = pa.DataFrameSchema(
 
 
 ndjson_filename = Path(__file__).parents[1] / "test_luigi_examples" / "data" / "flat-randomusers.txt"
-# read as a LazyFrame, since want to test pandera against a LazyFrame rather than a DataFrame
-lf = pl.LazyFrame(pl.read_ndjson(ndjson_filename))
+# schema validation and checks work with a polars DataFrame; checks don't seem to work with a polars LazyFrame
+df = pl.read_ndjson(ndjson_filename)
+# make 2 copies of the same data to see that we get multiple validation fails
+df = pl.concat([df, df], rechunk=True)
 
 try:
-    results = schema.validate(lf, lazy=True).collect()
+    results = schema.validate(df, lazy=True)
 except pa.errors.SchemaErrors as exc:
     print("Schema errors and failure cases:")
     print(exc.failure_cases)
     print("\nDataFrame object that failed validation:")
-    print(exc.data)
-
-print(results)
+    print(exc.data.collect())
